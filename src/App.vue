3cb0ef4f9e889b4e2a4e3700239b2e712a9bc8b1
Kae <73745621+Kae-Tempest@users.noreply.github.com>
2022-04-07 08:30:06 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Fligth_data></Fligth_data>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Fligth_data from './components/Flight'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Fligth_data
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Flight.vue
@@ -0,0 +1,36 @@
+<template>
+<ul>
+  <li v-for="post in posts" :key="post">{{post[1]}} {{post[2]}}</li>
+</ul>
+</template>
+
+<script>
+export default {
+  name: "Flight_data",
+  methods: {
+    async getData() {
+      try{
+        let res = await fetch('https://opensky-network.org/api/states/all');
+        let data = await res.json();
+        console.log(data)
+        this.posts = data['states']
+        this.time = data['time']
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+  data() {
+    return {
+      posts: []
+    }
+  },
+  mounted() {
+    this.getData()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
